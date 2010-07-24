162d27bd6dbdc87263ed0bd1abcd69fb52948773
Brian Guthrie <bguthrie@thoughtworks.com>
2010-07-25 02:09:19 +0530

    Renamed a URL, added a badge, some formatting tweaks.

--- github-badges/spec/github_badges_spec.rb
+++ github-badges/spec/github_badges_spec.rb
@@ -7,17 +7,17 @@ describe "Github badges" do
 
   it "should render a user's name when you request their badge page" do
     Octopi::User.stubs(:find).with("cv").returns stub(:name => "Carlos", :public_repo_count => 0)
-    get "/users/cv"
-    last_response.body.should have_tag(".user .name", /Carlos/)
+    get "/badges/cv"
+    last_response.body.should have_tag(".user .intro", /Carlos/)
 
     Octopi::User.stubs(:find).with("bguthrie").returns stub(:name => "Brian", :public_repo_count => 0)
-    get "/users/bguthrie"
-    last_response.body.should have_tag(".user .name", /Brian/)
+    get "/badges/bguthrie"
+    last_response.body.should have_tag(".user .intro", /Brian/)
   end
 
   it "should not render any badges if the user matches no badges" do
     Octopi::User.stubs(:find).with("cv").returns stub(:name => "cv", :public_repo_count => 0)
-    get "/users/cv"
+    get "/badges/cv"
     last_response.body.should_not have_tag("ul#badges")
     last_response.body.should have_tag(".sadpanda", "The user you've requested does not have any badges.")
   end
@@ -26,11 +26,11 @@ describe "Github badges" do
     Badge.add(:name => "Has a repo") { |user| user.public_repo_count == 1 }
 
     Octopi::User.stubs(:find).with("cv").returns stub(:name => "cv", :public_repo_count => 1)
-    get "/users/cv"
+    get "/badges/cv"
     last_response.body.should have_tag("ul#badges li.badge", /Has a repo/)
 
     Octopi::User.stubs(:find).with("bguthrie").returns stub(:name => "bguthrie", :public_repo_count => 0)
-    get "/users/bguthrie"
+    get "/badges/bguthrie"
     last_response.body.should_not have_tag("ul#badges li.badge", /Has a repo/)
   end
 
@@ -38,7 +38,7 @@ describe "Github badges" do
     Badge.add(:name => "Truth-haver", :description => "Word") { true }
 
     Octopi::User.stubs(:find).with("foo").returns stub(:name => "foo")
-    get "/users/foo"
+    get "/badges/foo"
     last_response.body.should have_tag("ul#badges li.badge .name", "Truth-haver")
     last_response.body.should have_tag("ul#badges li.badge .description", "Word")
   end
