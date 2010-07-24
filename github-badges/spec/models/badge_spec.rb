83360e06b4eab199932104023fb7b35008cfcb20
Brian Guthrie <bguthrie@thoughtworks.com>
2010-07-24 23:25:09 +0530

    App now has basic styles. Badge names and descriptions. Added additional badges.

--- github-badges/spec/models/badge_spec.rb
+++ github-badges/spec/models/badge_spec.rb
@@ -3,21 +3,27 @@ require 'spec_helper'
 describe Badge do
   context "name" do
     it "should have a short name" do
-      Badge.new("Happy User").name.should == "Happy User"
+      Badge.new(:name => "Happy User").name.should == "Happy User"
+    end
+  end
+
+  context "description" do
+    it "should have a description" do
+      Badge.new(:description => "Unhappy user").description.should == "Unhappy user"
     end
   end
 
   context "applicable_to?" do
     it "should always be true if given block is true" do
-      Badge.new("test") { true }.should be_applicable_to(:anything)
+      Badge.new { true }.should be_applicable_to(:anything)
     end
 
     it "should always be false if given block is false" do
-      Badge.new("test") { false }.should_not be_applicable_to(:anything)
+      Badge.new { false }.should_not be_applicable_to(:anything)
     end
 
     it "should evaluate the block against the given object" do
-      threepwood = Badge.new("test") do |user|
+      threepwood = Badge.new do |user|
         user.name == "threepwood"
       end
 
@@ -31,21 +37,21 @@ describe Badge do
 
     it "should add the given badge to the list of badges" do
       Badge.should_not have_badge("a badge")
-      Badge.add("a badge") {}
+      Badge.add(:name => "a badge") {}
       Badge.should have_badge("a badge")
       Badge.all.size.should == 1
     end
 
     it "should be able to clear the list of badges" do
-      Badge.add("a badge")
+      Badge.add(:name => "a badge") {}
       Badge.clear_badges!
       Badge.all.should be_empty
     end
 
     it "should be able to filter a list of badges applicable to a user" do
-      happy  = Badge.add("a badge")       { |u| u.happy? }
-      yellow = Badge.add("another badge") { |u| u.color =~ /yellow/ }
-      green  = Badge.add("yet a third")   { |u| u.color =~ /green/ }
+      happy  = Badge.add { |u| u.happy? }
+      yellow = Badge.add { |u| u.color =~ /yellow/ }
+      green  = Badge.add { |u| u.color =~ /green/ }
 
       Badge.filter(stub(:happy? => true, :color => "red")).should == [ happy ]
       Badge.filter(stub(:happy? => false, :color => "yellow-green")).should == [ yellow, green ]
