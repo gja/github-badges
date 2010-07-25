922f56afd318710821a3d6e5a7cb5779a5f38cb7
Sidu Ponnappa <ckponnappa@gmail.com>
2010-07-25 07:16:05 +0530

    more js

--- anything2/config/routes.rb
+++ anything2/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.answer '/answer', :controller => 'answer', :action => 'index'
+  map.answerf '/answer', :controller => 'answer', :action => 'index'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
--- anything2/public/javascripts/maps.js
+++ anything2/public/javascripts/maps.js
@@ -10,43 +10,47 @@
           // Add the Pan control to have North, South, East and West directional control  
           map.addPanControl();    
           // Specifying the Map starting location and zoom level  
-          map.drawZoomAndCenter("San Francisco", 3);  
-          // Add an event to report to our Logger  
-          YEvent.Capture(map, EventsList.MouseClick, myCallback);  
+				var first = questions[0].question;
+        map.drawZoomAndCenter(new YGeoPoint(first.latitude, first.longitude), 11);  
+          // Add an event to report to our Logger
+
+        function createMarker(question){  
+          //var mapCoordCenter = map.convertLatLonXY(map.getCenterLatLon());  
+          currentGeoPoint = new YGeoPoint(question.latitude, question.longitude);
+          var newMarker= new YMarker(currentGeoPoint);  
+          // newMarker.addAutoExpand("Add a Label to a Marker for this Effect");  
+					var markerMarkup = "<span class='question'><b>"+ question.text  +"</b> <input type='text' class='answer'><a class='submit'>submit</a></span>";  
+					newMarker.openSmartWindow(markerMarkup);
+					return newMarker;
+				}  
     
-          function myCallback(_e, _c){  
-              /*   
-                 It is optional to specify the location of the Logger.   
-                 Do so by sending a YCoordPoint to the initPos function.  
-               */  
-              var mapCoordCenter = map.convertLatLonXY(map.getCenterLatLon());  
-              currentGeoPoint = new YGeoPoint( _c.Lat, _c.Lon);  
-              placeMarker(currentGeoPoint);  
-              displayPolyLines(currentGeoPoint);    
-          }  
-    
-          function placeMarker(geoPoint){  
-            var newMarker= new YMarker(geoPoint);  
-            // newMarker.addAutoExpand("Add a Label to a Marker for this Effect");  
-            newMarker.addLabel("Foo Woo boo");  
-						var markerMarkup = "<span class='question'><b>You can add markup this?</b> <input type='text' class='answer'><a class='submit'>submit</a></span>";  
-						newMarker.openSmartWindow(markerMarkup);
-						// YEvent.Capture(newMarker, EventsList.MouseClick, function(){ newMarker.openSmartWindow(markerMarkup); });
-            map.addOverlay(newMarker);
-						var context = "#" + newMarker.id;
-						$(context + " .answer").click(function(){console.log( $(context + " input").focus());})
-            $(context + " .submit").click(function(){console.log( $(context + " input").val());});
-          }  
+        function placeMarker(map, newMarker){
+					// YEvent.Capture(newMarker, EventsList.MouseClick, function(){ newMarker.openSmartWindow(markerMarkup); });
+          map.addOverlay(newMarker);
+					var context = "#" + newMarker.id;
+					$(context + " .answer").click(function(){console.log( $(context + " input").focus());})
+          $(context + " .submit").click(function(){console.log( $(context + " input").val());});
+					displayPolyLines(newMarker.YGeoPoint);
+        }  
     
           function displayPolyLines(g_point){  
               polylinePoints.push(g_point);  
               if (canDisplayPolyLines){  
-                  map.addOverlay(new YPolyline(polylinePoints, 'black',7,0.7));  
+                map.addOverlay(new YPolyline(polylinePoints, 'black',7,0.7));  
               }  
           }  
     
           this.canDisplayPolyLines = function() {  
-              // Check to make sure we have at least 2 points to connect  
-              return (polylinePoints.length > 1);  
-          }  
+            // Check to make sure we have at least 2 points to connect  
+            return (polylinePoints.length > 1);  
+          }
+				
+				
+				var markers = $.map(questions, function(question, i){
+					return createMarker(map, question.question, question.question);
+				}); 
+
+				$.each(markers, function(i, marker){
+					placeMarker(map, marker);
+				});     
       }
