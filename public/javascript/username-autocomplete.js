95be6aaf2b89fb5312781654a7340721a7283edd
Brian Guthrie <bguthrie@thoughtworks.com>
2010-07-25 13:16:09 +0530

    User search now attempts to autocomplete with YQL. Spinner to show what's what.

---
+++ public/javascript/username-autocomplete.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var ENV = "http://blazing-water-96.heroku.com/yql/github.env?v=1";
+
+  $("#username").autocomplete({
+    source: function(request, response) {
+      $("#spinner").show();
+      $.queryYQL("select * from github.user.search where login = \"" + request.term + "\"", ENV, function(data) {
+        var results = $(data.query.results.users.user).map(function() { return this.username; });
+        response(results);
+        $("#spinner").hide();
+      });
+    }
+  });
+});
+
