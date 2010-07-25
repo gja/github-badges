928054701d690fb1b053e4482182e196f92119e6
Niranjan Paranjape <niranjan@c42.in>
2010-07-25 08:17:07 +0530

    hack

--- anything2/config/routes.rb
+++ anything2/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.answerf '/answer', :controller => 'answer', :action => 'index'
+  map.answer '/answer', :controller => 'answer', :action => 'index'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
